f4df4d6d78825f4d3e8e889c3732adca27f1a4be
Chuma Mcphoy <chmcphy@gmail.com>
2015-08-24 19:39:09 +0200

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,10 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+shoe_size = raw_input("What is you're shoe size? ")
+
+print "So, you're %r old, %r tall and length of your shoe is %s." % (
+age, height, shoe_size)
+
+# So basically this is a more concise method of conveying same info as in ex11.
+
+# Also check out the various pydoc commands (open, file, os etc.)
