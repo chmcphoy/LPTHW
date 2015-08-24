7718899b64749efabf63d395c81649be0c13c3e1
Chuma Mcphoy <chmcphy@gmail.com>
2015-08-24 03:44:23 +0200

    Update ex3.py
    Updated the comments in the exercise

--- ex3.py
+++ ex3.py
@@ -1,14 +1,17 @@
 print "I will now count my chickens:" 
- 
-print "Hens", 25.0 + 30.0 / 6.0 # since the math q is after "..", only the result is displayed after "Hens",
-print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # same (answer in number)
+
+# since the math q is after "..", only the result is displayed after "Hens", 
+print "Hens", 25.0 + 30.0 / 6.0 
+# same (answer in number)
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 
  
 print "Now I will count the eggs:" 
+
+# No quotation marks here, only answer in bash
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 
  
-print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # No quotation marks here, only answer in bash
- 
-print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?" # Here the math q is part of the "..", thus question will also be displayed
- 
+# Here the math q is part of the "..", thus question will also be displayed
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
 print 3.0 + 2.0 < 5.0 - 7.0 
  
 print "What is 3.0 + 2.0?", 3.0 + 2.0 
