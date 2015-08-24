b2947f4fec660c93429ec696419109b36edcc379
Chuma Mcphoy <chmcphy@gmail.com>
2015-08-24 19:37:30 +0200

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,14 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+print "What are you're 2 favorite movies of all time?",
+movies = raw_input()
+
+print "%s? Hmm, sounds interesting ill have to check them out!" % movies
