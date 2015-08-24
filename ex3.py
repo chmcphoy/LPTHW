32b69b673594c1f1989673c107ee5eaaa89b665f
Chuma Mcphoy <chmcphy@gmail.com>
2015-08-24 03:42:34 +0200

    Create ex3.py
    Added Exercise 3.

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens:" 
+ 
+print "Hens", 25.0 + 30.0 / 6.0 # since the math q is after "..", only the result is displayed after "Hens",
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # same (answer in number)
+ 
+print "Now I will count the eggs:" 
+ 
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # No quotation marks here, only answer in bash
+ 
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?" # Here the math q is part of the "..", thus question will also be displayed
+ 
+print 3.0 + 2.0 < 5.0 - 7.0 
+ 
+print "What is 3.0 + 2.0?", 3.0 + 2.0 
+print "What is 5.0 - 7.0?", 5.0 - 7.0 
+ 
+print "Oh, that's why it's False." 
+ 
+print "How about some more." 
+ 
+print "Is it greater?", 5.0 > -2.0 
+print "Is it greater or equal?", 5.0 >= -2 
+print "Is it less or equal?", 5.0 <= -2.0 
